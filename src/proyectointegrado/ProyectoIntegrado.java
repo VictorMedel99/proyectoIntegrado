ce7036a0811e0cbf9cc2f4fa82de6bca263dc40d
victorm <victorm@victorM>
2020-07-22 12:40:12 -0500

    se muestran algunos patrones de diseño

---
+++ src/proyectointegrado/ProyectoIntegrado.java
@@ -0,0 +1,16 @@
+package proyectointegrado;
+
+/**
+ *
+ * @author victorm
+ */
+public class ProyectoIntegrado {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
